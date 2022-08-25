f8f580f6a756bb890ee1fd42c70040b78ed75fda
Krasskoo <krasimirbogdanov99@gmail.com>
2022-08-25 16:31:56 +0300

    Trying some checks with wordlist file.

--- AnagramTS_/dist/app.js
+++ AnagramTS_/dist/app.js
@@ -7,11 +7,22 @@ const express_1 = __importDefault(require("express"));
 const WordList_json_1 = __importDefault(require("./WordList.json"));
 const app = (0, express_1.default)();
 const port = 3000;
+const sorting = (stringg) => {
+    return stringg.split("").sort().join("");
+};
+const inputString = sorting("melon");
+//let inputStringLength: number = inputString.length;
 app.get("/", (req, res) => {
     const wordListLenght = WordList_json_1.default.length;
     // res.send(`<b>${words}</b>`);
-    // res.send("I'm trying to pass some news into browser here!");
-    res.send(`<br>${wordListLenght}`);
+    for (let i = 0; i < wordListLenght; i++) {
+        if (inputString === sorting(WordList_json_1.default[i])) {
+            res.send(`<br>${WordList_json_1.default[i]}`);
+        }
+        //throw new Error("Do not match!");
+        //res.send(`<b>${words}</b>`);
+        console.log(sorting(WordList_json_1.default[i]));
+    }
 });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
--- AnagramTS_/src/app.ts
+++ AnagramTS_/src/app.ts
@@ -3,14 +3,24 @@ import words from "./WordList.json";
 const app = express();
 const port = 3000;
 
-let inputString: string;
-let inputStringLength: number;
+const sorting = (stringg) => {
+  return stringg.split("").sort().join("");
+};
 
+const inputString: string = sorting("melon");
+//let inputStringLength: number = inputString.length;
 
 app.get("/", (req, res) => {
   const wordListLenght = words.length;
- // res.send(`<b>${words}</b>`);
-  res.send(`<br>${wordListLenght}`);
+  // res.send(`<b>${words}</b>`);
+  for (let i = 0; i < wordListLenght; i++) {
+    if (inputString === sorting(words[i])) {
+      res.send(`<br>${words[i]}`);
+    }
+    //throw new Error("Do not match!");
+    //res.send(`<b>${words}</b>`);
+    console.log(sorting(words[i]));
+  }
 });
 
 app.listen(port, () => {
