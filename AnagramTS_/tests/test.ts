5f9a8810fe73cddb4a5ffbddd1f961b747f87d6f
Krasskoo <krasimirbogdanov99@gmail.com>
2022-09-14 15:29:15 +0300

    test is passed, but found 1 open handle

--- AnagramTS_/tests/test.ts
+++ AnagramTS_/tests/test.ts
@@ -1,7 +1,7 @@
 import { fillMatchedWords } from '../src/app';
  
 describe('testing anagram', () => {
-  test('inputed word "melon" should matched 4 words', () => {
+  test('inputed word "melon" should matched 4 words ', () => {
     expect(fillMatchedWords().length).toBe(4);
   });
 });
