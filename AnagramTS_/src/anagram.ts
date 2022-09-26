4192427e4fb553adb3b9a95d4b07b78d4abc8f7f
Krasskoo <krasimirbogdanov99@gmail.com>
2022-09-26 14:50:51 +0300

    remove index and replace with different type of loop

--- AnagramTS_/src/anagram.ts
+++ AnagramTS_/src/anagram.ts
@@ -1,9 +1,6 @@
-//import express from "express";
 import words from "./WordList.json";
-//const app = express();
-//const port = 3000;
 
-const sorting = (stringg) => {
+const sorting = (stringg: string) => {
   return stringg.split("").sort().join("");
 };
 
@@ -11,38 +8,22 @@ export function fillMatchedWords() {
   const inputString: string = sorting("melon");
   const inputStringLength: number = inputString.length;
 
-  const wordListLength: number = words.length;
   const matchedWordsList: string[] = [];
 
-  for (let i = 0; i < wordListLength; i++) {
-    // console.log("I:"+i);
-    if (inputStringLength == words[i].length) {
-      if (inputString != sorting(words[i])) continue;
-      matchedWordsList.push(words[i]);
+  for (const word of words) {
+    if (inputStringLength === word.length) {
+      if (inputString !== sorting(word)) continue;
+      matchedWordsList.push(word);
     } else
-      for (let j = 0; j < wordListLength; j++) {
-        const nextWordOfListLength: number = words[j].length;
-        /*console.log(
-          "NextWordOfList: " + "i:" + i + " j:" + j + " is " + words[j]
-        );*/
-        const concatenatedWordsLength: number =
-          words[i].length + nextWordOfListLength;
+      for (const scdWord of words) {
+        const concatenatedWordsLength: number = word.length + scdWord.length;
         if (inputStringLength != concatenatedWordsLength) continue;
-        const concatenatedWordsSorted: string = sorting(words[i] + words[j]);
+        const concatenatedWordsSorted: string = sorting(word + scdWord);
         if (inputString != concatenatedWordsSorted) continue;
-        matchedWordsList.push(words[i] + " + " + words[j]);
+        matchedWordsList.push(word + " + " + scdWord);
       }
   }
-  // console.log("Number of matched words are: " + matchedWordsList.length);
+
   return matchedWordsList;
 }
 console.log("Matched words are: " + fillMatchedWords());
-/*
-app.get("/", (req, res) => {
-  res.send(`${fillMatchedWords()}`);
-});
- 
-app.listen(port, async () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
-*/
