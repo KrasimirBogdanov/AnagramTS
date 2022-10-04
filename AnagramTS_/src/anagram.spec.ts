a252717387088af63381d224838e0ea92e8fd60d
Krasskoo <krasimirbogdanov99@gmail.com>
2022-10-04 15:31:57 +0300

    rebuild the test to suit the class

--- AnagramTS_/src/anagram.spec.ts
+++ AnagramTS_/src/anagram.spec.ts
@@ -1,7 +1,7 @@
-import { fillMatchedWords } from './anagram';
- 
-describe('testing anagram', () => {
-  test('inputed word "melon" should matched 4 words ',() => {
-    expect(fillMatchedWords().length).toBe(4);
+import { wordToCheck } from "./anagram";
+
+describe("testing anagram", () => {
+  test('inputed word "melon" should matched 4 words ', () => {
+    expect(wordToCheck.checkIsFirstWordAnagram().length).toBe(4);
   });
 });
